b65f46759374b73efbe239eeabd892a9e8faadc9
simon <simondu06730@hotmail.fr>
2023-11-25 15:56:03 +0100

    deplor

--- src/App.jsx
+++ src/App.jsx
@@ -6,22 +6,21 @@ import { Environment } from "@react-three/drei";
 
 import "../src/App.css";
 
-import { Dino } from "./Dino";
-import Model from "./Scene";
-import Me from "../Me";
-import { Background } from "../Background";
+import {Me} from "../public/components/Me";
 import { useFrame } from "@react-three/fiber";
 import { useRef } from "react";
 function Scene() {
   const ref = useRef(null);
   useFrame(() => {
-    ref.current.rotation.y = 30;
-    ref.current.position.y = -3;
+    ref.current.rotation.y = 4.5;
+    ref.current.position.y = -20;
+    ref.current.position.x = 18;
+    ref.current.position.z = 10;
   });
 
   return (
     <>
-      <mesh ref={ref} scale={[3, 3, 3]}>
+      <mesh ref={ref} scale={[15, 15, 15]}>
         <Me />
       </mesh>
     </>
@@ -30,18 +29,16 @@ function Scene() {
 
 export default function App() {
   return (
-    <div className={{overflow:"hidden"}}> 
-       <Canvas   camera={{ position: [-5, 2, 2] }}>
+    <div className="overflow-hidden">
+      <Canvas camera={{ position: [-20, 2, 5] }}>
         <OrbitControls />
         <ambientLight intensity={0.5} />
-        <spotLight position={[25, 25, 30]} angle={0.9} />
+        <spotLight position={[0, 0, 0]} angle={0.9} />
         <Suspense>
           <Scene />
         </Suspense>
-        <Environment files="./Models/space.hdr" background></Environment>
+        <Environment files="./Models/ship1.hdr" background></Environment>
       </Canvas>
-      <p>coucou les ami</p>
-      </div>  
-    
+    </div>
   );
 }
