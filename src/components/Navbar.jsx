174887175bbf87b089f8013a178a7394e2a16e1b
simon <simondu06730@hotmail.fr>
2023-11-27 22:57:42 +0100

    deploy

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,7 +7,7 @@ const Navbar = () => {
       <div className="flex  ">
         <nav className="flex p-10  text-lg bg-black  [clip-path-polygon(0_3%,_75%_0%,_100%_50%,_75%_100%,_0_100%,_0%_50%)] uppercase">
         <ul className=" flex justify-center flex-col text-white ">
-          <li className=' relative [clip-path:circle(70%_at_50%_30%)]'><a className="  text-center hover:cursor-pointer hover:" href=''>Accueil</a><span className=" before:content-[''] before:w-[150px] before:rotate-45 before:h-[1px] before:bg-lime-800 before:absolute before:top-[85px] before:left-[10px] hover:before:bg-blue hover:before:w-[150px] hover:before:h-[1px] "></span></li>
+          <li className=' relative [clip-path:circle(70%_at_50%_30%)]'><a className="relative text-center hover:cursor-pointer before:content-[''] before:w-[0px] before:bg-lime-600 before:h-[1px] before:rotate-45 before:absolute hover:before:w-[100px] hover:before:top-[62px] hover:before:transition-[width]  duration-150 ease-in-out" href=''>Accueil</a></li>
           <li><a className="hover:cursor-pointer" href=''>Mes projets</a></li>
           <li><a className="hover:cursor-pointer" href=''>Contact</a></li>
 
