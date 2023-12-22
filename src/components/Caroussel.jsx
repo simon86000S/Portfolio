2867afe81a96c55b349980267f27c96a824afd92
simon <simondu06730@hotmail.fr>
2023-12-22 23:50:29 +0100

    deploy

--- src/components/Caroussel.jsx
+++ src/components/Caroussel.jsx
@@ -1,9 +1,11 @@
 import React, { useRef, useState } from "react";
+import { Link } from "react-router-dom";
 // Import Swiper React components
 import { Swiper, SwiperSlide } from "swiper/react";
 
 // Import Swiper styles
 import "swiper/css";
+
 import "swiper/css/free-mode";
 import "swiper/css/pagination";
 import debrou from "../assets/debrou.mp4";
@@ -21,36 +23,47 @@ const Caroussel = () => {
           clickable: true,
         }}
         modules={[FreeMode, Pagination, Autoplay]}
-        className=" Swiper mt-40 pl-10 flex flex-col  "
+        className="mt-[70px] pl-10 max-sm:w-[100vh] pl-0   "
       >
         <SwiperSlide
-          className="  text-white h-[500px] rounded-md flex flex-col bg-black  "
+          className="  text-white h-[590px] rounded-md flex flex-col bg-black  "
           data-swiper-autoplay="2000"
         >
-          <video autoplay loop muted className="relative  rounded-md">
+          <video autoplay loop  muted className="relative">
             <source src={debrou} type="video/mp4" />
           </video>
        
           <img className="absolute top-0" src={navBar}/>
-          <button className=" bg-lime-600 rounded-full text-xl mt-2">Aller sur le site</button>
+          <Link to='https://montanes-service-r4ru48cm5-sims-projects-d92c89a3.vercel.app' className=" bg-lime-600 rounded-full text-xl mt-2 text-center">Aller sur le site</Link>
           <h1 className="text-center text-xl text-lime-600">Site Vitrine</h1>
          
-          <div className="flex flex-col p-5 ">
+          <div className="flex flex-col p-2 ">
           <h3 className="underline decoration-solid ">Présentation d’ensemble du projet:</h3>
               <p>Montanes Service est une entreprise à deux activitées: Espace vert et de Nettoyage de vitre</p>
-            <h3>Objectifs:</h3> 
+            <h3 className="underline decoration-solid ">Missions:</h3> 
+            <p>-Création du Logo </p>
+            <p>-Design du site</p>
+            <p>-Respect de la charte graphique </p>
+            <p>-Recueil d'avis clients</p>
+            <p>-Formulaire de contact</p>
+            <div>
+              <h1 className="underline decoration-solid ">Technologie utilisés:</h1>
+              <p>-ViteJs</p>
+              <p>-Canvas</p>
             </div>
+            </div>
+           
 
 
         </SwiperSlide>
         <SwiperSlide
-          className="bg-white h-[500px] rounded-md "
+          className="bg-white h-[580px] rounded-md "
           data-swiper-autoplay="2000"
         >
           Slide 2
         </SwiperSlide>
         <SwiperSlide
-          className="bg-white h-[500px] rounded-md"
+          className="bg-white h-[580px] rounded-md"
           data-swiper-autoplay="2000"
         >
           Slide 3
