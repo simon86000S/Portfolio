b9f468af26164f44bb06ae79630bc37768c337f6
simon <simondu06730@hotmail.fr>
2024-01-07 22:13:52 +0100

    deploy

--- src/components/MyProject.jsx
+++ src/components/MyProject.jsx
@@ -8,7 +8,7 @@ const MyProject = () => {
   const [active, setActive] = useState(true);
   const navigation = [
     { name: "ViteJs", Link: "/", current: true },
-    { name: "NodeJs", Link: "Caroussel_Back", current: false },
+    { name: "NodeJs", Link: "/Caroussel_Back", current: false },
     { name: "NextJs", Link: "#", current: false },
     { name: "MySQL", Link: "#", current: false },
   ];
@@ -16,12 +16,12 @@ const MyProject = () => {
     return classes.filter(Boolean).join(" ");
   }
   return (
-    <div id="Project" className=" z-50  bg-slate-200 shadow-xl shadow-black pt-10">
+    <div id="Project" className="   bg-slate-200 shadow-xl shadow-black pt-10">
       <div className="relative">
         <h1 className='min-md:relative  text-8xl before:content-["Portfolio"] before:text-black before:opacity-5 before:absolute before:top-10 before:left-[30%] before:text-9xl max-sm:text-5xl max-md:before:text-5xl max-sm:before:text-[50px] before:left-25% flex justify-center max-sm:before:bottom-0  '>
           Mes Projets
         </h1>
-        <div className="lg:p-20 m-10  z-50 ">
+        <div className="lg:p-20 m-10 max-sm:p-0 max-sm:mt-10 max-sm:m-0  ">
           {" "}
           {navigation.map((item) => (
             <Link
@@ -29,8 +29,8 @@ const MyProject = () => {
               to={item.Link}
               className={classNames(
                 item.current
-                  ? " p-5 bg-black text-white z-50 ml-5 max-sm:p-2 max-sm:m-2 hover:bg-slate-700 "
-                  : " p-5 bg-black text-white z-50 ml-5 max-sm:p-2 max-sm:m-2 hover:bg-slate-700"
+                  ? " p-5 bg-black text-white  ml-5 max-sm:p-2 max-sm:m-2 hover:bg-slate-700 "
+                  : "  p-5 bg-black text-white  ml-5 max-sm:p-2 max-sm:m-2 hover:bg-slate-700"
               )}
               aria-current={item.current ? "page" : undefined}
             >
